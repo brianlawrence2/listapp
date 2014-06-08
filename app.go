15bc774151da3fe84db472ab7e2ba15095871d7b
Brian Lawrence <brianlawrence2@gmail.com>
2014-06-07 23:24:07 -0500

    go fmt

--- app.go
+++ app.go
@@ -1,31 +1,31 @@
 package main
 
 import (
-  "net/http"
-  "html/template"
-  )
+	"html/template"
+	"net/http"
+)
 
 type Page struct {
-  Title string
+	Title string
 }
 
 type Staff struct {
-  FName string
-  LName string
-  Facility string
-  EMail string
+	FName    string
+	LName    string
+	Facility string
+	EMail    string
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-  t, _ := template.ParseFiles("templates/main.html")
-  s := &Staff{FName: "Brian", LName: "Lawrence", Facility: "Hospital 1", EMail: "b@b.net"}
-  t.Execute(w, s)
+	t, _ := template.ParseFiles("templates/main.html")
+	s := &Staff{FName: "Brian", LName: "Lawrence", Facility: "Hospital 1", EMail: "b@b.net"}
+	t.Execute(w, s)
 }
 
 func main() {
-  http.HandleFunc("/", homeHandler)
-  http.HandleFunc("/static/", func(w http.ResponseWriter, r *http.Request) {
-    http.ServeFile(w, r, r.URL.Path[1:])
-    })
-  panic(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/static/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, r.URL.Path[1:])
+	})
+	panic(http.ListenAndServe(":8080", nil))
 }
