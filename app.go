f573c7d1462fe7b4847b2fb6987c93d8e9357d9d
Brian Lawrence <brianlawrence2@gmail.com>
2014-06-10 23:13:14 -0500

    added crappy edit handler

--- app.go
+++ app.go
@@ -17,19 +17,29 @@ type StaffMember struct {
 	FName    string
 	LName    string
 	Facility string
+	Position string
 	EMail    string
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("templates/main.html")
-	sm1 := StaffMember{FName: "Brian", LName: "Lawrence", Facility: "Hospital 1", EMail: "b@b.net"}
-	sm2 := StaffMember{FName: "Clark", LName: "Lawrence", Facility: "Hospital 2", EMail: "c@c.net"}
+	sm1 := StaffMember{FName: "Brian", LName: "Lawrence", Facility: "Hospital 1", Position: "CEO", EMail: "b@b.net"}
+	sm2 := StaffMember{FName: "Clark", LName: "Lawrence", Facility: "Hospital 2", Position: "CFO", EMail: "c@c.net"}
+	s := &Staff{StaffMembers: []StaffMember{sm1, sm2}}
+	t.Execute(w, s)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("templates/edit.html")
+	sm1 := StaffMember{FName: "Brian", LName: "Lawrence", Facility: "Hospital 1", Position: "CEO", EMail: "b@b.net"}
+	sm2 := StaffMember{FName: "Clark", LName: "Lawrence", Facility: "Hospital 2", Position: "CFO", EMail: "c@c.net"}
 	s := &Staff{StaffMembers: []StaffMember{sm1, sm2}}
 	t.Execute(w, s)
 }
 
 func main() {
 	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/edit/", editHandler)
 	http.HandleFunc("/static/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, r.URL.Path[1:])
 	})
